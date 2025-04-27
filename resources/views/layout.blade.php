2334715aa73ab4d2987066cf8088cebde5964281
abhijeetkumar097 <aabhijeet097@gmail.com>
2025-04-27 14:11:50 +0530

    updated layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>My Project</title>
+    <title>AT_WASTE_MANAGEMENT</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
 </head>
@@ -11,7 +11,7 @@
     {{-- Navbar --}}
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">MyApp</a>
+            <a class="navbar-brand" href="#">AT_WASTE_MANAGEMENT</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
